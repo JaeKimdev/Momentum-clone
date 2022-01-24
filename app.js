de455586b10f01348bbecbdb0f6be676b5bf57cf
Jae Hyung Kim <smfc.mene@gmail.com>
2022-01-24 23:03:40 +0800

    Update App

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
+const loginInput = loginForm.querySelector("input"); // same as const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector("#greeting");
 
-// same as line above
-// const loginInput = document.querySelector('#login-form input');
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기
   // event object는 preventDefault함수를 기본적으로 갖고 있음
-  console.log(event);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
+
+console.log(savedUsername);
