7e785b7e2eddbbc1f03027ddab9c8915487bf066
Jae Hyung Kim <smfc.mene@gmail.com>
2022-01-25 15:55:50 +0800

    Update clock.js
    Using padStart to shows two digits in seconds area.

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0"); // PadStart (digit, "character to fill") ,padEnd fill in characters at the back.
+  const seconds = String(date.getSeconds()).padStart(2, "0"); // padStart(자리수, "채울 문자"), padEnd는 뒤쪽에 문자를 채운다.
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
