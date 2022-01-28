476c9e6c018ef1ad90224565033699287c443980
Jae Hyung Kim <smfc.mene@gmail.com>
2022-01-28 14:56:48 +0800

    Add ToDo function - Delete

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); // same as document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
+function (event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", dlelteToDo)
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
