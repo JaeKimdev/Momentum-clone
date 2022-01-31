baf2d9e7d16103b155ca469ed2937e822bd7eaf0
Jae Hyung Kim <smfc.mene@gmail.com>
2022-01-31 20:32:16 +0800

    Update todo.js
    Fix to show localStorage array items when refresh

--- js/todo.js
+++ js/todo.js
@@ -4,13 +4,13 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function dlelteToDo(event) {
+function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
@@ -21,7 +21,7 @@ function paintToDo(newTodo) {
   span.innerText = newTodo;
   const button = document.createElement("button");
   button.innerText = "❌";
-  button.addEventListener("click", dlelteToDo);
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
@@ -42,5 +42,6 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
