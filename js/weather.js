bc9c274f24505c2ce76dadfd1176bc8c8ccb2097
Jae Hyung Kim <84453688+mene79@users.noreply.github.com>
2022-02-05 21:27:43 +0800

    Update css files

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "c3a0d766208ddddc0955bdf4d566b12f";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const log = position.coords.longitude;
+  const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
